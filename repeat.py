d274847129d8ba5ce4bbf60492c6f10c21dee316
Mihai Morariu <mihaimorariu@gmail.com>
2012-03-03 18:57:57 +0100

    Friends/enemies lists

--- repeat.py
+++ repeat.py
@@ -5,7 +5,7 @@ red = blu = 0
 
 for i in range(MATCHES):
     settings = core.Settings(think_time = 0.0625, max_steps=600)
-    game = core.Game('old2.py', 'agent_rulebased.py', rendered = False, settings = settings, verbose = False)
+    game = core.Game('old.py', 'agent_rulebased.py', rendered = False, settings = settings, verbose = False)
     game.run()
     s = game.stats.score
     if s > 0.55:
