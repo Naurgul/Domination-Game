4b200f7b0fa3801b7e23874ca7ff40ee3cd87170
Mihai Morariu <mihaimorariu@gmail.com>
2012-03-07 20:59:17 +0100

    Updates:
    The agent tries to escape if it gets stuck somewhere
    Removed exploration, since the ammopacks are found almost instantly

--- runme.py
+++ runme.py
@@ -2,5 +2,5 @@ from domination import core
 
 settings = core.Settings(think_time = 0.625, max_steps=600)
 drawGraphics = True
-game = core.Game('old.py', 'agent_rulebased.py', rendered = drawGraphics, settings = settings, verbose = True)
+game = core.Game('agent_rulebased.py', 'old.py', rendered = drawGraphics, settings = settings, verbose = True)
 game.run()
