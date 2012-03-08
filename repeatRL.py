aa931eaa5eca6741df57643a4a49d145cff5d5f0
VassilisM <vasilismaragos@yahoo.com>
2012-03-08 19:32:27 +0100

    added vm_speedbiases functions, read remarks

--- repeatRL.py
+++ repeatRL.py
@@ -1,11 +1,11 @@
 from domination import core
 
-MATCHES = 100
+MATCHES = 20
 red = blu = 0
 
 for i in range(MATCHES):
     settings = core.Settings(think_time = 0.0925, max_steps=500)
-    game = core.Game('old.py', 'agent_rulebased.py', rendered = False, settings = settings, verbose = False)
+    game = core.Game('agent_reinforcement_learning.py', 'agent_reinforcement_learning.py', rendered = False, settings = settings, verbose = False)
     game.run()
     s = game.stats.score
     if s > 0.55:
